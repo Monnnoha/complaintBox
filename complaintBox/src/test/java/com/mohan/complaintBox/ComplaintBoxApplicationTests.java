a5ff828ce66c1e68838386b029e9fdb474f838a4
Monnnoha <chinthapallimohan19@gmail.com>
2026-01-31 14:13:02 +0530

    init commit

---
+++ complaintBox/src/test/java/com/mohan/complaintBox/ComplaintBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohan.complaintBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
